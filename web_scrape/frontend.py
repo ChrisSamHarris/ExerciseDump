dd84026fc05d61b842056706bd453b4134b142aa
christopher-harris_msmg <christopher.harris@moneysupermarket.com>
2023-09-22 15:31:26 +0100

    SQL interaction added with web_scrape module and exercise

--- web_scrape/frontend.py
+++ web_scrape/frontend.py
@@ -1,11 +1,27 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+import sqlite3
 
+connection = sqlite3.connect("temp_data.db")
 
 st.set_page_config(page_title="Average World Temperature", layout="centered", page_icon="🌡️")
 st.title("Average World Temperature")
 
-df = pd.read_csv("data.txt")
-figure = px.line(x=df['date'], y=df['temperature'], labels={"x":'date', "y":'temperature'})
+# df = pd.read_csv("data.txt")
+cursor = connection.cursor()
+
+cursor.execute("SELECT date FROM temperature")
+dates = cursor.fetchall()
+dates = [item[0] for item in dates]
+print(dates)
+
+cursor.execute("SELECT temperature FROM temperature")
+temps = cursor.fetchall()
+temps = [item[0] for item in temps]
+print(temps)
+
+
+figure = px.line(x=dates, y=temps, 
+                 labels={"x":'date', "y":'temperature'})
 st.plotly_chart(figure)
--- web_scrape/scrape_data.py
+++ web_scrape/scrape_data.py
@@ -2,10 +2,13 @@ import requests
 import selectorlib
 import time
 from datetime import datetime
-import os
+import sqlite3
+import os.path
 
 URL = "https://programmer100.pythonanywhere.com/"
 filename = "data.txt"
+
+connection = sqlite3.connect("temp_data.db")
 
 def get_time():
     current_time = datetime.now()
@@ -25,18 +28,15 @@ def extract(source):
 
 def store(ext_data):
     current_date = get_time()
-    
-    if not os.path.exists(filename):
-        with open(filename, 'w') as file:
-            file.write("date,temperature\n")
-            print(f"{filename} successfully created!")
 
-    with open(filename, 'a') as file:
-        file.write(f"{current_date},{ext_data}\n")
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO temperature VALUES(?,?)", (current_date, int(ext_data)))
+    connection.commit()
 
 
 if __name__ == "__main__":
     while True:
+
         generature_data = input("Would you like more sample data? (y/n)")
         if generature_data.strip().lower() == "y":
             for i in range(1,4):
